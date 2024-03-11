03e5e0e24a8beee1d376aabade859da3b1bf5153
MrEshboboyev <mreshboboyev@gmail.com>
2024-03-11 22:02:24 +0500

    Copying Dto models to ToDo.Web Project

---
+++ ToDo.Web/Models/RegistrationRequestDto.cs
@@ -0,0 +1,12 @@
+﻿namespace ToDo.Web.Models
+{
+    public class RegistrationRequestDto
+    {
+        public string Email { get; set; }
+        public string Nickname { get; set; }
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Password { get; set; }
+        public string? Role { get; set; }
+    }
+}
