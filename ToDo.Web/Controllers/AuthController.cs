03fb4c85d8a73c74ba792b6be726023514167029
MrEshboboyev <mreshboboyev@gmail.com>
2024-03-11 22:48:59 +0500

    AuthController.cs create and action [HttpGet] : Login, Logout, Register; _Layout.cshtml updated for [IsAuthenticated] condition

---
+++ ToDo.Web/Controllers/AuthController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Mvc;
+using ToDo.Web.Models;
+
+namespace ToDo.Web.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            LoginRequestDto loginRequestDto = new();
+            return View(loginRequestDto);
+        }
+        
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+        
+        [HttpGet]
+        public IActionResult Logout()
+        {
+            return View();  
+        }
+
+
+    }
+}
