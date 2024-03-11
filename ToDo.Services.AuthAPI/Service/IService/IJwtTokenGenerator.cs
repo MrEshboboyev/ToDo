a345ba77a3315bd283c51566e16fcd31fe23028f
MrEshboboyev <mreshboboyev@gmail.com>
2024-03-11 21:21:18 +0500

    Creating Services/JwtTokenGenerator and Services/IServices/IJwtTokenGenerator.cs

---
+++ ToDo.Services.AuthAPI/Service/IService/IJwtTokenGenerator.cs
@@ -0,0 +1,9 @@
+﻿using ToDo.Services.AuthAPI.Model;
+
+namespace ToDo.Services.AuthAPI.Service.IService
+{
+    public interface IJwtTokenGenerator
+    {
+        string GenerateJwtToken();
+    }
+}
---
+++ ToDo.Services.AuthAPI/Service/JwtTokenGenerator.cs
@@ -0,0 +1,13 @@
+﻿using ToDo.Services.AuthAPI.Model;
+using ToDo.Services.AuthAPI.Service.IService;
+
+namespace ToDo.Services.AuthAPI.Service
+{
+    public class JwtTokenGenerator : IJwtTokenGenerator
+    {
+        public string GenerateJwtToken()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
