6341fb34cda0dba15df8a8cb9ef22e2b4bf8fdaf
MrEshboboyev <mreshboboyev@gmail.com>
2024-03-11 21:23:19 +0500

    Configute JwtTokenGenerator for lifetime; Inject JwtOptions in JwtTokenGenerator

--- ToDo.Services.AuthAPI/Program.cs
+++ ToDo.Services.AuthAPI/Program.cs
@@ -25,6 +25,7 @@ builder.Services.Configure<JwtOptions>(builder.Configuration.GetSection("ApiSett
 
 // lifetime service
 builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddScoped<IJwtTokenGenerator, JwtTokenGenerator>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
--- ToDo.Services.AuthAPI/Service/JwtTokenGenerator.cs
+++ ToDo.Services.AuthAPI/Service/JwtTokenGenerator.cs
@@ -1,10 +1,19 @@
-﻿using ToDo.Services.AuthAPI.Model;
+﻿using Microsoft.Extensions.Options;
+using ToDo.Services.AuthAPI.Model;
+using ToDo.Services.AuthAPI.Models;
 using ToDo.Services.AuthAPI.Service.IService;
 
 namespace ToDo.Services.AuthAPI.Service
 {
     public class JwtTokenGenerator : IJwtTokenGenerator
     {
+        private readonly JwtOptions _jwtOptions;
+
+        public JwtTokenGenerator(IOptions<JwtOptions> jwtOptions)
+        {
+            _jwtOptions = jwtOptions.Value;
+        }
+
         public string GenerateJwtToken()
         {
             throw new NotImplementedException();
