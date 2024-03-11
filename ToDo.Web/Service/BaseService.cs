ab58c58368c6da8691ef732474b2cd45fde0f181
MrEshboboyev <mreshboboyev@gmail.com>
2024-03-11 22:28:33 +0500

    SendAsync() method full writed

--- ToDo.Web/Service/BaseService.cs
+++ ToDo.Web/Service/BaseService.cs
@@ -1,13 +1,85 @@
-﻿using ToDo.Web.Models;
+﻿using Newtonsoft.Json;
+using System.Net;
+using System.Text;
+using ToDo.Web.Models;
 using ToDo.Web.Service.IService;
+using static ToDo.Web.Utility.SD;
 
 namespace ToDo.Web.Service
 {
     public class BaseService : IBaseService
     {
-        public Task<ResponseDto?> SendAsync(RequestDto requestDto)
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public BaseService(IHttpClientFactory httpClientFactory)
         {
-            throw new NotImplementedException();
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<ResponseDto?> SendAsync(RequestDto requestDto)
+        {
+            try
+            {
+                var client = _httpClientFactory.CreateClient("ToDoAPI");
+                HttpRequestMessage message = new();
+                message.Headers.Add("Accept", "application/json");
+
+                // token
+
+                message.RequestUri = new Uri(requestDto.Url);
+                if (requestDto.Data != null)
+                {
+                    message.Content = new StringContent(JsonConvert.SerializeObject(requestDto.Data), Encoding.UTF8, "application/json");
+                }
+
+                HttpResponseMessage? apiResponse = null;
+
+                switch (requestDto.ApiType)
+                {
+                    case ApiType.POST:
+                        message.Method = HttpMethod.Post;
+                        break;
+                    case ApiType.DELETE:
+                        message.Method = HttpMethod.Delete;
+                        break;
+                    case ApiType.PUT:
+                        message.Method = HttpMethod.Put;
+                        break;
+                    default:
+                        message.Method = HttpMethod.Get;
+                        break;
+                }
+
+                apiResponse = await client.SendAsync(message);
+
+                switch (apiResponse.StatusCode)
+                {
+                    case HttpStatusCode.NotFound:
+                        return new ResponseDto() { IsSuccess = false, Message = "Not Found" };
+                    case HttpStatusCode.Unauthorized:
+                        return new ResponseDto() { IsSuccess = false, Message = "Unauthorized" };
+                    case HttpStatusCode.InternalServerError:
+                        return new ResponseDto() { IsSuccess = false, Message = "Internal Server Error" };
+                    case HttpStatusCode.Forbidden:
+                        return new ResponseDto() { IsSuccess = false, Message = "Access Denied" };
+                    default:
+                        var apiContent = await apiResponse.Content.ReadAsStringAsync();
+                        var apiResponseDto = JsonConvert.DeserializeObject<ResponseDto>(apiContent);
+                        return apiResponseDto;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                var dto = new ResponseDto()
+                {
+                    IsSuccess = false,
+                    Message = ex.Message,
+                };
+
+                return dto;
+            }
+
         }
     }
 }
