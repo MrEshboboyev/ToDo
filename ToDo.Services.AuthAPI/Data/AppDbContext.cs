25b8f82ac53a3bf5099b405476e1837546c321d0
MrEshboboyev <mreshboboyev@gmail.com>
2024-03-11 14:01:45 +0500

    Add ApplicationUser.cs model and applying database

--- ToDo.Services.AuthAPI/Data/AppDbContext.cs
+++ ToDo.Services.AuthAPI/Data/AppDbContext.cs
@@ -1,12 +1,21 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using ToDo.Services.AuthAPI.Model;
 
 namespace ToDo.Services.AuthAPI.Data
 {
-    public class AppDbContext : DbContext
+    public class AppDbContext : IdentityDbContext<ApplicationUser>
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
             
+        }
+
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
         }
     }
 }
---
+++ ToDo.Services.AuthAPI/Models/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace ToDo.Services.AuthAPI.Model
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string? Name { get; set; }
+        public string? Nickname { get; set; }
+    }
+}
