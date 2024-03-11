e565d14bd1870ec63fce41f5492dba4ac56eba2d
MrEshboboyev <mreshboboyev@gmail.com>
2024-03-11 13:54:12 +0500

    Database configuring in AuthAPI

--- ToDo.Services.AuthAPI/Program.cs
+++ ToDo.Services.AuthAPI/Program.cs
@@ -1,6 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using ToDo.Services.AuthAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// add database connection for AuthAPI project
+builder.Services.AddDbContext<AppDbContext>(option =>
+{
+    option.UseNpgsql(builder.Configuration.GetConnectionString("AuthConnection"));
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
