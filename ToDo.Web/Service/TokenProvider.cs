642aa4fa0119832aff4706de85aecc08e7517310
MrEshboboyev <mreshboboyev@gmail.com>
2024-03-12 09:37:48 +0500

    DI for HttpContextAccessor in TokenProvider.cs

--- ToDo.Web/Service/TokenProvider.cs
+++ ToDo.Web/Service/TokenProvider.cs
@@ -4,6 +4,13 @@ namespace ToDo.Web.Service
 {
     public class TokenProvider : ITokenProvider
     {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public TokenProvider(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
         public void ClearToken()
         {
             throw new NotImplementedException();
